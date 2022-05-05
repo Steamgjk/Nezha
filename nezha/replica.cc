2eac2e9e03c0543daf7d4c299e88fb1f74fc6cf7
root <root@opensource-nezha.us-central1-a.c.jinkun-pro1.internal>
2022-05-05 18:49:25 +0000

    implement early buffer

--- nezha/replica.cc
+++ nezha/replica.cc
@@ -19,41 +19,30 @@ namespace nezha {
             << "\treplicaId=" << replicaId_
             << "\treplicaNum=" << replicaNum_;
 
-        CreateContext();
+        CreateMasterContext();
+        CreateReceiverContext();
         // Launch Threads (based on Config)
         LaunchThreads();
-
-        // Register Master Timer 
-        ev_init(&masterTimer_, [](struct ev_loop* loop, struct ev_timer* w, int revents) {
-            static_cast<Replica*>(w->data)->Master();
-            });
-
-        ev_timer_again(masterLoop_, &masterTimer_);
-
-        // Register Master IO
-        ev_io_init(&masterIO_, [](struct ev_loop* loop, struct ev_io* w, int revents) {
-            static_cast<Replica*>(w->data)->MasterReceive();
-            }, masterSocketFd_, EV_READ);
-        ev_io_start(masterLoop_, &masterIO_);
-
-        ev_run(masterLoop_, 0);
+        ev_run(evLoops_["master"], 0);
     }
 
     Replica::~Replica()
     {
-
         for (auto& kv : threadPool_) {
             delete kv.second;
             LOG(INFO) << "Deleted\t" << kv.first;
         }
     }
-    void Replica::CreateContext() {
-        masterLoop_ = ev_loop_new();
-        masterTimer_.repeat = replicaConfig_["main-loop-period-ms"].get<uint32_t>() * 1e-3;
-        masterTimer_.data = (void*)this;
-        masterIO_.data = (void*)this;
-        masterSocketFd_ = socket(PF_INET, SOCK_DGRAM, 0);
-        if (masterSocketFd_ < 0) {
+    void Replica::CreateMasterContext() {
+        char* buffer = new char[BUFFER_SIZE];
+        struct ev_loop* evLoop = ev_default_loop();
+        struct ev_timer* evTimer = new ev_timer();
+        evTimer->repeat = replicaConfig_["main-loop-period-ms"].get<uint32_t>() * 1e-3;
+        evTimer->data = (void*)this;
+        struct ev_io* evIO = new ev_io();
+        evIO->data = (void*)this;
+        int fd = socket(PF_INET, SOCK_DGRAM, 0);
+        if (fd < 0) {
             LOG(ERROR) << "masterSocketFd_ fail";
         }
         struct sockaddr_in addr;
@@ -62,55 +51,149 @@ namespace nezha {
         addr.sin_port = htons(replicaConfig_["master-ports"][replicaId_].get<uint32_t>());
         addr.sin_addr.s_addr = inet_addr(replicaConfig_["replica-ips"][replicaId_].get<std::string>().c_str());
         // Bind socket to address
-        if (bind(masterSocketFd_, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
+        if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
             LOG(ERROR) << "bind error";
         }
+        // Register Master Timer 
+        ev_init(evTimer, [](struct ev_loop* loop, struct ev_timer* w, int revents) {
+            static_cast<Replica*>(w->data)->Master();
+            });
 
+        ev_timer_again(evLoop, evTimer);
+
+        // Register Master IO
+        ev_io_init(evIO, [](struct ev_loop* loop, struct ev_io* w, int revents) {
+            static_cast<Replica*>(w->data)->MasterReceive(w->fd);
+            }, fd, EV_READ);
+        ev_io_start(evLoop, evIO);
+
+        std::string key("master");
+        evLoops_[key] = evLoop;
+        evIOs_[key] = evIO;
+        evTimers_[key] = evTimer;
+        socketFds_[key] = fd;
+        receiverBuffers_[key] = buffer;
+    }
+
+    void Replica::CreateReceiverContext() {
         for (int i = 0; i < replicaConfig_["receiver-shards"]; i++) {
-            std::string key("receiverTd-" + std::to_string(i));
-            evLoops_[key] = ev_loop_new();
+            char* buffer = new char[BUFFER_SIZE];
+            struct ev_loop* evLoop = ev_loop_new();
+            struct ev_io* evIO = new ev_io();
+            int fd = socket(PF_INET, SOCK_DGRAM, 0);
+            if (fd < 0) {
+                LOG(ERROR) << "Receiver Fd fail " << i;
+            }
+            struct sockaddr_in addr;
+            bzero(&addr, sizeof(addr));
+            addr.sin_family = AF_INET;
+            addr.sin_port = htons(replicaConfig_["receiver-ports"][replicaId_].get<uint32_t>() + i);
+            addr.sin_addr.s_addr = inet_addr(replicaConfig_["replica-ips"][replicaId_].get<std::string>().c_str());
+            // Bind socket to address
+            if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
+                LOG(ERROR) << "bind error";
+            }
+            ev_io_init(evIO, [](struct ev_loop* loop, struct ev_io* w, int revents) {
+                int* idPtr = (int*)(w->data);
+                static_cast<Replica*>(w->data)->RequestReceive(*idPtr, w->fd);
+                }, fd, EV_READ);
+            ev_io_start(evLoop, evIO);
+
+            std::string key("ReceiveTd-" + std::to_string(i));
+            evLoops_[key] = evLoop;
+            evIOs_[key] = evIO;
+            socketFds_[key] = fd;
+            receiverBuffers_[key] = buffer;
+            requestBuffers_.push_back(buffer);
         }
     }
     void Replica::LaunchThreads() {
         // RequestReceive
         for (int i = 0; i < replicaConfig_["receiver-shards"]; i++) {
-            std::thread* td = new std::thread(&Replica::RequestReceive, this, i);
-            std::string key("receiverTd-" + std::to_string(i));
+            std::thread* td = new std::thread(&Replica::ReceiveTd, this, i);
+            std::string key("ReceiveTd-" + std::to_string(i));
             threadPool_[key] = td;
             LOG(INFO) << "Launched " << key << "\t" << td->native_handle();
         }
         // RequestProcess
         for (int i = 0; i < replicaConfig_["process-shards"]; i++) {
-            std::thread* td = new std::thread(&Replica::RequestProcess, this, i);
-            std::string key("processTd-" + std::to_string(i));
+            std::thread* td = new std::thread(&Replica::ProcessTd, this, i);
+            std::string key("ProcessTd-" + std::to_string(i));
             threadPool_[key] = td;
             LOG(INFO) << "Launched " << key << "\t" << td->native_handle();
         }
         // RequestReply
         for (int i = 0; i < replicaConfig_["reply-shards"]; i++) {
-            std::thread* td = new std::thread(&Replica::RequestReply, this, i);
-            std::string key("replyTd-" + std::to_string(i));
+            std::thread* td = new std::thread(&Replica::ReplyTd, this, i);
+            std::string key("ReplyTd-" + std::to_string(i));
             threadPool_[key] = td;
             LOG(INFO) << "Launched " << key << "\t" << td->native_handle();
         }
 
     }
-    void Replica::RequestReceive(int id) {
+
+    void Replica::ReceiveTd(int id) {
         workerCounter_.fetch_add(1);
+        std::string key("ReceiveTd-" + std::to_string(id));
+        ev_run(evLoops_[key], 0);
+        workerCounter_.fetch_sub(1);
+    }
+    void Replica::ProcessTd(int id) {
+        workerCounter_.fetch_add(1);
+        uint64_t reqKey = 0;
+        while (status_ == NORMAL) {
+            if (processQu_.try_dequeue(reqKey)) {
+                Request* req = requestMap_.get(reqKey);
+                if (req != NULL) {
+                    uint64_t deadline = req->sendtime() + req->bound();
+                    std::pair<uint64_t, uint64_t>myEntry(deadline, reqKey);
+                    if (myEntry > lastReleasedEntryByKeys_[req->key()]) {
+                        earlyBuffer_.insert(myEntry);
+                    }
+                    else {
+                        // req cannot enter early buffer
+                        if (AmLeader()) {
+                            // leader modifies its deadline
+                            // followers donot care about it (i.e. leave it in late buffer)
+
+                        }
+                    }
+
+                }
+                else {
+                    LOG(WARNING) << "Abnormal behavior: req is NULL";
+                }
+            }
+
+            // Polling early-buffer
+        }
+        workerCounter_.fetch_sub(1);
+    }
+    void Replica::ReplyTd(int id) {
+        workerCounter_.fetch_add(1);
+        int fd = socketFds_["ReplyTd-" + std::to_string(id)];
+        while (status_ == NORMAL) {
+
+        }
+        workerCounter_.fetch_sub(1);
+    }
+    void Replica::RequestReceive(int id, int fd) {
         struct sockaddr_in addr;
         socklen_t addrlen;
-        workerCounter_.fetch_sub(1);
+        char* buffer = requestBuffers_[id];
+        int sz = recvfrom(fd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &addrlen);
+        if (sz > 0) {
+            // Parse and process message
+            Request* request = new Request();
+            if (request->ParseFromArray(buffer, sz)) {
+                uint64_t reqKey = request->clientid();
+                reqKey = ((reqKey << 32u) | (request->reqid()));
+                requestMap_.assign(reqKey, request);
+                processQu_.enqueue(reqKey);
+            }
+        }
     }
-    void Replica::RequestProcess(int id) {
-        workerCounter_.fetch_add(1);
 
-        workerCounter_.fetch_sub(1);
-    }
-    void Replica::RequestReply(int id) {
-        workerCounter_.fetch_add(1);
-
-        workerCounter_.fetch_sub(1);
-    }
     void Replica::Master() {
         uint64_t nowTime = GetMicrosecondTimestamp();
         // I have not heard from the leader for a long time, start a view change
@@ -124,20 +207,21 @@ namespace nezha {
             // Increment viewNum
             viewNum_++;
             // Stop masterTimer
-            ev_timer_stop(masterLoop_, &masterTimer_);
+            ev_timer_stop(evLoops_["master"], evTimers_["master"]);
 
             // Start ViewChange
             StartViewChange();
         }
     }
-    void Replica::MasterReceive() {
+    void Replica::MasterReceive(int fd) {
         struct sockaddr_in addr;
         socklen_t addrlen;
-        int sz = recvfrom(masterSocketFd_, masterBuffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &addrlen);
+        char* buffer = receiverBuffers_["master"];
+        int sz = recvfrom(fd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &addrlen);
         if (sz > 0) {
             // Parse and process message
             Request* request = new Request();
-            if (request->ParseFromArray(masterBuffer, sz)) {
+            if (request->ParseFromArray(buffer, sz)) {
                 requestMap_.assign(request->sendtime(), request);
             }
         }
--- nezha/replica.h
+++ nezha/replica.h
@@ -40,19 +40,33 @@ namespace nezha {
         std::atomic<uint32_t> replicaId_;
         std::atomic<uint32_t> replicaNum_;
         std::atomic<uint32_t> status_; // for workers to check whether they should stop (for view change)
+
+        std::set<std::pair<uint64_t, uint64_t>> earlyBuffer_; // <deadline, reqKey>
+        std::vector<std::pair<uint64_t, uint64_t>> lastReleasedEntryByKeys_;
+
+
         // use <threadName> as the key (for search), <threadPtr, threadId> as the value
         std::map<std::string, std::thread*> threadPool_;
+
+        // Context
         std::map<std::string, struct ev_loop*> evLoops_;
+        std::map<std::string, struct ev_io*> evIOs_;
+        std::map<std::string, struct ev_timer*> evTimers_;
+        std::map<std::string, int> socketFds_;
+        std::map<std::string, char*> receiverBuffers_;
+        std::vector<char*> requestBuffers_;
+
         std::atomic<uint64_t> lastHeartBeatTime_; // for master to check whether it should issue view change
         std::atomic<uint32_t> workerCounter_; // for master to check whether everybody has stopped
-        struct ev_loop* masterLoop_;
-        int masterSocketFd_;
-        struct ev_io masterIO_;
-        struct ev_timer masterTimer_;
-        char masterBuffer[BUFFER_SIZE];
 
-        ConcurrentMap<uint64_t, void*> requestMap_;
-        ConcurrentMap<uint64_t, void*> replyMap_;
+
+        ConcurrentMap<uint64_t, Request*> requestMap_;
+        ConcurrentMap<uint64_t, Reply*> replyMap_;
+        ConcurrentQueue<uint64_t> processQu_;
+        ConcurrentQueue<uint64_t> replyQus_[16];
+
+        void CreateMasterContext();
+        void CreateReceiverContext();
         void CreateContext();
         void LaunchThreads();
         void StartViewChange();
@@ -60,11 +74,14 @@ namespace nezha {
     public:
         Replica(const std::string& configFile = std::string("../configs/nezha-replica.config.json"));
         ~Replica();
-        void RequestReceive(int id);
-        void RequestProcess(int id);
-        void RequestReply(int id);
+
+        void ReceiveTd(int id = -1);
+        void ProcessTd(int id = -1);
+        void ReplyTd(int id = -1);
+        void RequestReceive(int id = -1, int fd = -1);
+
         void Master();
-        void MasterReceive();
+        void MasterReceive(int fd = -1);
     };
 
 
