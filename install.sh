159050765e564416afc9217ab57bc5d511c1ef83
root <root@opensource-nezha.us-central1-a.c.jinkun-pro1.internal>
2022-06-15 20:40:21 +0000

    rm install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install -y net-tools autoconf libtool build-essential protobuf-compiler pkg-config
-git clone https://github.com/enki/libev.git
-chmod -R 777 libev
-cd libev && sudo ./autogen.sh 
-./configure && make && sudo make install
