cf32d527898686cedf061984cdd3832e7e5673a8
katie gioioso <katie@clockwork.io>
2023-09-19 01:33:51 +0000

    run local replicas/proxies/client and make sure at least one request is committed

---
+++ scripts/local_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export FLAGS_alsologtostderr=1
+
+echo "Launching replica 0..."
+($HOME/Nezha/bazel-bin/replica/nezha_replica --config $HOME/Nezha/configs/local/nezha-replica-config-0.yam & )
+
+echo "Launching replica 1..."
+($HOME/Nezha/bazel-bin/replica/nezha_replica --config $HOME/Nezha/configs/local/nezha-replica-config-1.yaml  &)
+
+echo "Launching replica 2..."
+($HOME/Nezha/bazel-bin/replica/nezha_replica --config $HOME/Nezha/configs/local/nezha-replica-config-2.yaml &)
+
+echo "Launching proxy..."
+($HOME/Nezha/bazel-bin/proxy/nezha_proxy --config $HOME/Nezha/configs/local/nezha-proxy-config.yaml &)
+
+echo "Launching client..."
+$HOME/Nezha/bazel-bin/client/nezha_client --config $HOME/Nezha/configs/local/nezha-client-config.yaml
+
+# Kill replicas
+trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
+
+# TODO(Katie): This is currently only checking if at least one request succeeded. 
+# It does not check if the client/replica/proxy failed for some reason
+file="Client-Stats-1"
+if [ -e "$file" ]; then
+    line_count=$(wc -l < "$file")
+    if [ "$line_count" -le 1 ]; then
+        echo "File '$file' exists but has only one line."
+        echo "No successful requests."
+        exit 1 
+    else
+        echo "File '$file' exists and has more than one line."
+        exit 0
+    fi
+else
+    echo "File '$file' does not exist."
+    exit 1
+fi
